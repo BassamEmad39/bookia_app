139c283bf8d5604520daf7d5248b1fb843da5ef7
BassamEmad39 <besoemad16@gmail.com>
2025-07-05 00:43:11 +0300

    added shimmer and skeletonizer

--- lib/features/home/presentation/widgets/best_seller_list.dart
+++ lib/features/home/presentation/widgets/best_seller_list.dart
@@ -1,6 +1,8 @@
+import 'package:bookie_app/core/utils/app_colors.dart';
 import 'package:bookie_app/core/utils/text_styles.dart';
 import 'package:bookie_app/features/home/data/model/best_seller_response/product.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class BestSellerList extends StatelessWidget {
   const BestSellerList({super.key, required this.products});
@@ -13,29 +15,35 @@ class BestSellerList extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Best Seller', style: TextStyles.getHeadLine2()),
-          GridView.builder(
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemCount: products.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
-              mainAxisExtent: 250,
+          Skeletonizer(
+            enabled: products.isEmpty,
+            containersColor: AppColors.borderColor,
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: products.isEmpty ? 4 : products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                mainAxisExtent: 250,
+              ),
+              itemBuilder: (context, index) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    products[index].image ?? '',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                );
+              },
             ),
-            itemBuilder: (context, index) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.network(
-                  products[index].image??'',
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                ),
-              );
-            },
           ),
         ],
       ),
     );
   }
+
+  
 }
--- lib/features/home/presentation/widgets/home_slider.dart
+++ lib/features/home/presentation/widgets/home_slider.dart
@@ -3,6 +3,7 @@ import 'package:bookie_app/features/home/data/model/slider_response/slider.dart'
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeSlider extends StatefulWidget {
@@ -18,39 +19,41 @@ class _HomeSliderState extends State<HomeSlider> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CarouselSlider.builder(
-          itemCount: widget.sliders.length,
-          itemBuilder: (
-            BuildContext context,
-            int itemIndex,
-            int pageViewIndex,
-          ) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                widget.sliders[itemIndex].image??'',
-                fit: BoxFit.cover,
-                width: double.infinity,
+        widget.sliders.isEmpty
+            ? emptyUi()
+            : CarouselSlider.builder(
+              itemCount: widget.sliders.length,
+              itemBuilder: (
+                BuildContext context,
+                int itemIndex,
+                int pageViewIndex,
+              ) {
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    widget.sliders[itemIndex].image ?? '',
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  ),
+                );
+              },
+              options: CarouselOptions(
+                height: 150,
+                viewportFraction: 0.9,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                enlargeCenterPage: true,
+                enlargeFactor: 0.3,
+                scrollDirection: Axis.horizontal,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    activeIndex = index;
+                  });
+                },
               ),
-            );
-          },
-          options: CarouselOptions(
-            height: 150,
-            viewportFraction: 0.9,
-            initialPage: 0,
-            enableInfiniteScroll: true,
-            reverse: false,
-            autoPlay: true,
-            enlargeCenterPage: true,
-            enlargeFactor: 0.3,
-            scrollDirection: Axis.horizontal,
-            onPageChanged: (index, reason) {
-              setState(() {
-                activeIndex = index;
-              });
-            },
-          ),
-        ),
+            ),
         Gap(15),
         SmoothPageIndicator(
           controller: PageController(
@@ -68,6 +71,23 @@ class _HomeSliderState extends State<HomeSlider> {
           onDotClicked: (index) {},
         ),
       ],
+    );
+  }
+
+  emptyUi() {
+    return SizedBox(
+      width: double.infinity,
+      height: 150,
+      child: Shimmer.fromColors(
+        baseColor: AppColors.accentColor,
+        highlightColor: AppColors.primaryColor,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
     );
   }
 }
